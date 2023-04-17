1e4efa4fa026fd3a25e588e48a9a634f0f47b826
erfan <erfan@gmail.com>
2023-04-17 16:51:57 +0430

    layout wrapper fonts

--- src/App.js
+++ src/App.js
@@ -1,1 +1,11 @@
+import React from 'react'
+import Layout from './containers/layout/Layout'
+const App = () => {
+  return (
+    <Layout>
+      <div>shopping</div>
+    </Layout>
+  )
+}
 
+export default App
